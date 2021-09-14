1f365cfb3073edf743105229a622ffd5abb020dc
Nishant Sapkota <snishant306@gmail.com>
2021-09-14 10:57:11 +0545

    Made code a bit readable and removed redundant methods

--- ip-lookup.py
+++ ip-lookup.py
@@ -14,39 +14,28 @@ banner = """
 
 class whois:
     def __init__(self):
-        print(Fore.LIGHTWHITE_EX + banner)
-        print(Fore.LIGHTGREEN_EX + "'Hit Enter Key':'Your ipadd' or input someone's ip")
-        self.ipadd = (input(Fore.WHITE + "Input ip ~# ")).strip()
-        self.stripped_ip = str(self.ipadd)
+        print(f"{Fore.LIGHTWHITE_EX}{banner}")
+        print(f"{Fore.LIGHTGREEN_EX}Hit Enter Key':'Your ipadd' or input someone's ip")
+        self.ipadd = (input(Fore.WHITE + "Input IP ~# ")).strip()
 
-        if not self.stripped_ip == "":
-            if self.stripped_ip.startswith("127") or self.stripped_ip.startswith(
-                "192.168"
-            ):
-                print(Fore.RED + "This Are Not Valid Ip Address :(")
-                print(Fore.RED + "The Input Address is  local host")
-                exit()
-        else:
-            self.stripped_ip = ""
+        if self.ipadd.startswith("127") or self.ipadd.startswith("192.168"):
+            print(
+                f"{Fore.RED} The Input Address is  local host"
+                if self.ipadd.startswith("127")
+                else "This Are Not Valid Ip Address :("
+            )
+            exit()
 
-    def fetchinfo(self):
-        response = requests.get(url=("http://ip-api.com/json/" + self.ipadd)).text
-        # response = requests.get(
-        #     url=("http://ipwhois.app/json/" + self.stripped_ip)
-        # ).text  # for more info
-        json_response = json.loads(response)
-        return json_response
-
-    def printdata(self):
-        ip_details = self.fetchinfo()
-        for key, value in ip_details.items():
+    def print_fetched(self):
+        response = requests.get(f"http://ip-api.com/json/{self.ipadd}")
+        json_response = json.loads(response.text)
+        for key, value in json_response.items():
             if value:
                 print(
                     Fore.LIGHTWHITE_EX
-                    + f"{key.capitalize()}: {Fore.WHITE} {str(value).strip()}"
+                    + f"{key.title()}: {Fore.WHITE} {str(value).strip()}"
                 )
 
 
 obj = whois()
-obj.fetchinfo()
-obj.printdata()
+obj.print_fetched()
